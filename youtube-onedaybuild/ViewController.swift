40e37674233338bc8ad77c1e9e8a7200ad4ddba5
Lamin <aka_lamin@hotmail.com>
2020-08-10 00:42:14 +0200

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Lamin Tamba on 09/08/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
