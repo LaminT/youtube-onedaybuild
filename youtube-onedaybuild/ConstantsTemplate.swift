22a28198984eb10b7ae5ce0b857a6285d74da5f7
Lamin <aka_lamin@hotmail.com>
2020-08-12 00:49:09 +0200

    modified gitignore to stop tracking Constants.swift, added ConstantsTemplate.swift instead

--- youtube-onedaybuild/ConstantsTemplate.swift
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -1,15 +1,21 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youtube-onedaybuild
 //
-//  Created by Lamin Tamba on 10/08/2020.
+//  Created by Lamin Tamba on 12/08/2020.
 //
+
+/*
+ Add your own keys below and uncomment the code
+ */
 
 import Foundation
 
+/*
 struct Constants {
     static var API_KEY = ""
-    static var PLAYLIST_ID = "UUPUeTYyoAwQZ8H05T8dPLZA"
+    static var PLAYLIST_ID = ""
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(API_KEY)"
     
 }
+*/
