9ef7f5fe07c5feb052364822914adda92e23519c
Lamin <aka_lamin@hotmail.com>
2020-08-15 03:46:06 +0200

    added custom tableView cell, configured cell to show video thumbnail, title and date. Also added cache manager to save image data.

--- youtube-onedaybuild/Helpers/ConstantsTemplate.swift
+++ youtube-onedaybuild/Helpers/ConstantsTemplate.swift
@@ -11,11 +11,11 @@
 
 import Foundation
 
-
+/*
 struct Constants {
     static var API_KEY = ""
     static var PLAYLIST_ID = ""
     static var API_URL = "https:www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(API_KEY)"
     static var VIDEOCELL_ID = "VideoCell"
 }
-
+*/
